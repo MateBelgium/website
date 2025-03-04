7b458a818e2acf7f0cc7c28f80a9e986dc7a27f0
Elann <elann.fraiture@gmail.com>
2025-03-04 12:22:33 +0100

    random change

--- src/_data/products.js
+++ src/_data/products.js
@@ -11,39 +11,6 @@ function formatPrice(nbr, currency = "eur", locale = "fr-BE") {
   return formatter.format(decimalNbr);
 }
 
-/*
-async function getProducts() {
-  try {
-    const products = await stripe.products.list({
-      limit: 10,
-      expand: ["data.default_price"],
-    });
-
-    const formattedProducts = products.data.map((product) => {
-      return {
-        id: product.id,
-        name: product.name,
-        description: product.description,
-        priceId: product.default_price.id,
-        priceCurrency: product.default_price.currency,
-        priceAmount: product.default_price.unit_amount,
-        priceDisplay: formatPrice(product.default_price.unit_amount),
-        image: product.images[0] ?? null,
-        collection: product.metadata.collection ?? null,
-        quantityLeft: product.metadata.quantity ?? null,
-      };
-    });
-
-    console.log(formattedProducts)
-
-    return formattedProducts;
-  } catch (err) {
-    throw new Error(err);
-  }
-
-}
-  */
-
 async function getProducts() {
   try {
     const products = await stripe.products.list({
