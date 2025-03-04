ea0cadd948a21caaab16f0ebb69e7d32764efa1a
Elann <elann.fraiture@gmail.com>
2025-03-04 12:16:17 +0100

    Add | support for different sizes in stripe

--- src/_data/products.js
+++ src/_data/products.js
@@ -11,6 +11,7 @@ function formatPrice(nbr, currency = "eur", locale = "fr-BE") {
   return formatter.format(decimalNbr);
 }
 
+/*
 async function getProducts() {
   try {
     const products = await stripe.products.list({
@@ -32,11 +33,59 @@ async function getProducts() {
         quantityLeft: product.metadata.quantity ?? null,
       };
     });
+
+    console.log(formattedProducts)
+
     return formattedProducts;
   } catch (err) {
     throw new Error(err);
   }
 
+}
+  */
+
+async function getProducts() {
+  try {
+    const products = await stripe.products.list({
+      limit: 10,
+      expand: ["data.default_price"],
+    });
+
+    const productMap = new Map();
+
+    products.data.forEach((product) => {
+      const match = product.name.match(/^(.*)\s\[(.*)\]$/);
+      const baseName = match ? match[1] : product.name;
+      const size = match ? match[2] : null;
+
+      if (!productMap.has(baseName)) {
+        productMap.set(baseName, {
+          name: baseName,
+          description: product.description,
+          image: product.images[0] ?? null,
+          collection: product.metadata.collection ?? null,
+          sizes: [],
+        });
+      }
+
+      productMap.get(baseName).sizes.push({
+        id: product.id,
+        size: size || "One Size",
+        name: product.name,
+        priceId: product.default_price.id,
+        priceCurrency: product.default_price.currency,
+        priceAmount: product.default_price.unit_amount,
+        priceDisplay: formatPrice(product.default_price.unit_amount),
+        quantityLeft: product.metadata.quantity ?? null,
+      });
+    });
+
+    const formattedProducts = Array.from(productMap.values());
+    console.log(formattedProducts);
+    return formattedProducts;
+  } catch (err) {
+    throw new Error(err);
+  }
 }
 
 export default getProducts;
